5254579cfa8427f6a3cf0f966d4d274d583aa1ad
Stephen Done <srdone@gmail.com>
2017-05-19 22:26:35 -0600

    add recipients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 import { AngularFireAuth } from "angularfire2/auth";
 import { Observable } from "rxjs/Observable";
 import * as firebase from 'firebase/app';
+import { Subscription } from "rxjs/Subscription";
 
 interface Message {
   message: string,
-  isSent: boolean
+  isSent: boolean,
+  recipients: string[]
 }
 
 interface Recipient {
   phoneNumber: string,
-  name: string
+  name: string,
+  $key: string
 }
 
 @Component({
@@ -19,13 +22,15 @@ interface Recipient {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app works!';
   recipients: FirebaseListObservable<Recipient[]>;
   pendingMessages: FirebaseListObservable<Message[]>;
   user: Observable<firebase.User>;
-  newRecipient: Recipient = {name: undefined, phoneNumber: undefined};
+  newRecipient: Recipient = {name: undefined, phoneNumber: undefined, $key: undefined};
   newPendingMessage: string;
+  recipientKeys: string[];
+  recipientSubscription: Subscription
 
   constructor(
     public db: AngularFireDatabase,
@@ -36,6 +41,11 @@ export class AppComponent implements OnInit {
     this.recipients = this.db.list('/recipients');
     this.pendingMessages = this.db.list('/messages');
     this.user = this.afAuth.authState;
+    this.recipientSubscription = this.recipients.subscribe(snap => this.recipientKeys = snap.map(r => r.$key))
+  }
+
+  ngOnDestroy() {
+    this.recipientSubscription.unsubscribe();
   }
 
   login() {
@@ -53,7 +63,8 @@ export class AppComponent implements OnInit {
   addPendingMessage(message: string) {
     this.pendingMessages.push({
       message,
-      isSent: false
+      isSent: false,
+      recipients: this.recipientKeys
     });
   }
 
