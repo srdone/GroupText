3df2ddd3ad058c1c26547d820da0fd398572bef1
Stephen Done <srdone@gmail.com>
2017-05-20 07:46:30 -0600

    fix production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBP4I5pDwbi16HcyWgwGzqHkcH06bT8V5I",
+    authDomain: "grouptext-335c0.firebaseapp.com",
+    databaseURL: "https://grouptext-335c0.firebaseio.com",
+    projectId: "grouptext-335c0",
+    storageBucket: "grouptext-335c0.appspot.com",
+    messagingSenderId: "521916663956"
+  }
 };
